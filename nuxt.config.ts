e83ef1bda5f29b2b958c8480488c2d39e2e2ca6e
Franklin Tina <76698025+franklin-tina@users.noreply.github.com>
2026-01-17 23:58:15 -0600

    feat: enable Vite environment API in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   experimental: {
     typescriptPlugin: true,
+    viteEnvironmentApi: true,
   },
   modules: ["@nuxt/fonts", "@vite-pwa/nuxt"],
 
